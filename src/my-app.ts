bf65dfd4e0dc09688bdbfff88a1525e8310e4bfb
Scott Mobley <airboss001@gmail.com>
2020-07-26 08:14:31 -0500

    added environment file to hold app info

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,6 +1,11 @@
+import { version } from './environment';
 import 'alerty/dist/js/alerty.js'; //https://github.com/undead25/alerty
 import 'alerty/dist/css/alerty.css';
 
 export class MyApp
 {
+    constructor()
+    {
+        document.title = `Aurelia2-Sudoku ${version}`;
+    }
 }
